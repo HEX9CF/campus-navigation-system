3ba5c7d3ee6e2d898e9f7dd5f811d706d5220231
HEX9CF <hex9cf@aliyun.com>
2024-09-05 15:34:56 +0800

    添加数据模型

---
+++ nav/model/Edge.java
@@ -0,0 +1,13 @@
+package nav.model;
+
+/**
+ * 边
+ *
+ * @date 2024/09/05
+ */
+public class Edge {
+    private Integer id; // 边编号
+    private Integer Node1Id; // 结点1编号
+    private Integer Node2Id; // 结点2编号
+    private Integer distance; // 距离
+}
---
+++ nav/model/Node.java
@@ -0,0 +1,11 @@
+package nav.model;
+
+/**
+ * 结点
+ *
+ * @date 2024/09/05
+ */
+public class Node {
+    private Integer id; // 结点编号
+    private String name; // 结点名称
+}
